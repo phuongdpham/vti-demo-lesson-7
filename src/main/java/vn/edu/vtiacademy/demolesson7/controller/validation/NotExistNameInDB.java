92c40a7e6aabd8458945ac4b44a0fa5cbb95f4e1
phuongdpham <phamduyphuong10@gmail.com>
2023-09-07 20:58:26 +0700

    feat: add list to annotation

--- src/main/java/vn/edu/vtiacademy/demolesson7/controller/validation/NotExistNameInDB.java
+++ src/main/java/vn/edu/vtiacademy/demolesson7/controller/validation/NotExistNameInDB.java
@@ -4,21 +4,29 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
 @Constraint(validatedBy = NotExistNameInDBValidator.class)
-@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RUNTIME)
+@Repeatable(NotExistNameInDB.List.class)
 public @interface NotExistNameInDB {
     String message() default "{vn.edu.vtiacademy.validation.constraints.NotExistNameInDB.message}";
 
-    Class<?>[] groups() default { };
+    Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+    @Retention(RUNTIME)
+    @Documented
+    public @interface List {
+        NotExistNameInDB[] value();
+    }
 }
